ee6d3c555c0d8d0f48ed531738efe51fbe05d9c0
Joel951 <joeljames951@gamil.com>
2025-10-05 21:23:18 +0530

    initial commit

---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Navbar from "@/components/ui/home/header/navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
---
+++ components/ui/home/header/search-input.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchInput = () => {
+  return (
+    <div>
+      Search Inpt
+    </div>
+  )
+}
+
+export default SearchInput
