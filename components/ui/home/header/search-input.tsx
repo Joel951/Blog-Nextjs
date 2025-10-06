68d3c8c7c5ba9f29197c89f2bf08bda2680d39cb
joel.ballary <joel.ballary@jumpgrowth.com>
2025-10-06 21:38:28 +0530

    updated code - implemented clerk

--- components/ui/home/header/search-input.tsx
+++ components/ui/home/header/search-input.tsx
@@ -1,10 +1,15 @@
+import { Search } from 'lucide-react'
 import React from 'react'
+import { Input } from '../../input'
 
 const SearchInput = () => {
   return (
-    <div>
-      Search Inpt
+   <form action="">
+    <div className='relative'>
+      <Search className='absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground'/>
+      <Input type='text' placeholder='Search articles...' name='search' className='pl-10 w-48 focus-visible:ring-1'/>
     </div>
+   </form>
   )
 }
 
